da14ef2e77ca6cd46a944f9cc25cc834b4e6a45e
Rahul <rahulkumar1711@gmail.com>
2021-08-12 20:27:55 +0530

    first commit

---
+++ src/test/java/QaclickAcademy/AppiumTest.java
@@ -0,0 +1,14 @@
+package QaclickAcademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void NativeAppAnroid() {
+		System.out.println("NativeAppAnroid");
+	}
+	@Test
+	public void IosApps() {
+		System.out.println("IosApps");
+	}
+}
---
+++ src/test/java/QaclickAcademy/RestAPITest.java
@@ -0,0 +1,14 @@
+package QaclickAcademy;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	@Test
+	public void PostJira() {
+		System.out.println("PostJira");
+	}
+	@Test
+	public void DeleteTwitter() {
+		System.out.println("DeleteTwitter");
+	}
+}
